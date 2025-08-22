88f46a909a76a555d59f571aeeb627a9b14f87b9
Marianna <mariannacanchala451@gmail.com>
2025-08-22 17:56:50 -0500

    blucles

---
+++ Ejercicios_con_while_condicionales_estructuras.py
@@ -0,0 +1,57 @@
+#EJRCICIOS CON WHILE, CONDICIONALES Y ESTRUCTURAS
+#1. Suma hasta cero.
+# total = 0  # acumulador
+
+# while True:
+#     numero = int(input("Ingresa un número entero (0 para terminar): "))
+#     if numero == 0:
+#         break   # sale del bucle si el número es 0
+#     total += numero
+
+# print("La suma total es:", total)
+
+
+
+#2. Contraseña secreta
+# contraseña = ""
+# while contraseña == "Jirafa123":
+#     contraseña=input("Ingresa la contrseña")
+# print("Contraseña correcta")
+
+
+
+#3. Lista de compras
+# productos = []  # lista vacía para guardar los productos
+
+# while True:
+#     producto = input("Ingresa un producto (escribe 'fin' para terminar): ")
+#     if producto.lower() == "fin":  # si escribe fin, se detiene
+#         break
+#     productos.append(producto)  # agrega el producto a la lista
+
+# print("La lista de productos es:")
+# print(productos)
+
+
+
+#4. Contador de pares e impares
+# pares = 0
+# impares = 0
+# contador = 0
+
+# while contador < 10:   # se repite hasta que el usuario ingrese 10 números
+#     numero = int(input("Ingresa un número: "))
+    
+#     if numero % 2 == 0:   # si el residuo es 0, es par
+#         pares += 1
+#     else:
+#         impares += 1
+    
+#     contador += 1   # aumenta el conteo de números ingresados
+
+# print("Cantidad de números pares:", pares)
+# print("Cantidad de números impares:", impares)
+
+#ejercicios de bucles 
+
+
